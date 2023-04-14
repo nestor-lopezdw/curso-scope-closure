6b0e60f7f47a38c1f1dd7d887386c1966c03380b
nestor-lopezdw <lnextor2@gmail.com>
2023-04-14 08:40:33 -0500

    creacion package,json

---
+++ src/scope/global.js
@@ -0,0 +1,16 @@
+// variables
+
+var a;  // Declarando
+var b = "b"; // Declaramos y asignamos
+
+b = "bb";  // Reasignando
+var a = "aa";  // Redeclaración
+
+// Global scope
+var fruit = 'Apple';  // global
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
